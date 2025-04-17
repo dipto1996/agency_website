95dff59ba57be4c558bc6e4fe3c96f46b269a9ea
dipto1996 <roydiptopal1996@gmail.com>
2025-04-17 16:05:47 +0530

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,21 +4,31 @@ import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
 import ServicesSection from './components/ServicesSection';
-import ContactModal from './components/ContactModal';
+import Footer from './components/Footer';
+import ContactModal from './components/ContactModal'; // Make sure this component is already created
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   return (
-    <div className="App">
+    <div className="font-sans antialiased">
       <Navbar openModal={openModal} />
       <HeroSection openModal={openModal} />
       <AboutSection />
       <ServicesSection />
-      {isModalOpen && <ContactModal closeModal={closeModal} />}
+      
+      {/* Contact Modal */}
+      {modalOpen && <ContactModal closeModal={closeModal} />}
+
+      <Footer />
     </div>
   );
 }
